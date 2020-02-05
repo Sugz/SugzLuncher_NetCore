75c321c21ffa4625ca2cbdfb7a8286ea4f426bbd
Clément Plantec <plantec.clement@gmail.com>
2020-02-05 19:50:26 +0100

    Move IconUtils class

---
+++ SugzLuncher.Contracts/PluginViewModel.cs
@@ -0,0 +1,9 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SugzLuncher.Contracts
+{
+    public abstract class PluginViewModel : ViewModelBase { }
+}
--- SugzLuncher/Helpers/IconExtractor/IconUtils.cs
+++ SugzLuncher/Helpers/IconExtractor/IconUtils.cs
@@ -13,10 +13,6 @@ namespace SugzLuncher.Helpers
     internal static class IconUtils
     {
 
-
-
-
-
         internal static ImageSource FromIcon(Drawing.Icon icon, int size)
         {
             Drawing.Icon[] splitIcons = IconUtil.Split(icon);
@@ -25,11 +21,10 @@ namespace SugzLuncher.Helpers
 
         internal static ImageSource ExtractIcons(string path, int size, int index = 0)
         {
-            Drawing.Icon icon = null;
-            Drawing.Icon[] splitIcons = null;
-
+            Drawing.Icon[] splitIcons;
             try
             {
+                Drawing.Icon icon;
                 // Create an icon either from a .ico or from a .exe using IconExtractor
                 if (Path.GetExtension(path).ToLower() == ".ico")
                     icon = new Drawing.Icon(path);
@@ -49,30 +44,6 @@ namespace SugzLuncher.Helpers
 
             return SplitIcon(splitIcons, size);
 
-            //if (splitIcons != null)
-            //{
-            //    // Collect the sizes
-            //    int[] heights = new int[splitIcons.Length];
-            //    for (int i = 0; i < splitIcons.Length; i++)
-            //        heights[i] = splitIcons[i].Height;
-
-            //    // Find the closest size available (TODO: try the find the one bigger first)
-            //    int closestHeight = heights.Aggregate((x, y) => Math.Abs(x - size) < Math.Abs(y - size) ? x : y);
-
-            //    // Find the best layer that math the closest size and with the higher bit count
-            //    Drawing.Icon item = splitIcons.Where(x => x.Height == closestHeight).OrderByDescending(i => IconUtil.GetBitCount(i)).FirstOrDefault();
-            //    if (item != null)
-            //    {
-            //        using (Stream str = new MemoryStream())
-            //        {
-            //            item.Save(str);
-            //            str.Seek(0, SeekOrigin.Begin);
-            //            return new IconBitmapDecoder(str, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.OnLoad).Frames[0];
-            //        }
-            //    }
-            //}
-
-            //return null;
         }
 
 
@@ -92,12 +63,10 @@ namespace SugzLuncher.Helpers
                 Drawing.Icon item = splitIcons.Where(x => x.Height == closestHeight).OrderByDescending(i => IconUtil.GetBitCount(i)).FirstOrDefault();
                 if (item != null)
                 {
-                    using (Stream str = new MemoryStream())
-                    {
-                        item.Save(str);
-                        str.Seek(0, SeekOrigin.Begin);
-                        return new IconBitmapDecoder(str, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.OnLoad).Frames[0];
-                    }
+                    using Stream str = new MemoryStream();
+                    item.Save(str);
+                    str.Seek(0, SeekOrigin.Begin);
+                    return new IconBitmapDecoder(str, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.OnLoad).Frames[0];
                 }
             }
 
